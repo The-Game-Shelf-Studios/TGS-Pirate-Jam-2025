1e98715bbccf918eb17811b1cc4f8e0b581a92c4
mzolfo <32077843+mzolfo@users.noreply.github.com>
2025-01-21 16:58:54 -0600

    added sword enemy cube
    added a cube that can track the player's position, approach them at a certain distance and pause in place to attempt an attack

---
+++ Scripts/Enemies/EnemyMovementSword.gd
@@ -0,0 +1,61 @@
+extends CharacterBody3D
+
+
+@onready var PlayerNode = get_tree().get_first_node_in_group("Player")
+@export var SightRange: float = 10
+@export var AttackRange: float = 4
+@onready var MyPlayerTracker = find_child("PlayerTrackerNode")
+@export var SPEED = 0.5
+@export var AttackDamage: int
+@export var AttackDelay: float = 2.0
+var isAttacking = true
+var PlayerSeen = false
+
+func _ready() -> void:
+	PlayerSeen = false
+	isAttacking = false
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if PlayerNode != null:
+		if MyPlayerTracker.PlayerRelativeDistance <= SightRange && MyPlayerTracker.PlayerRelativeDistance >= AttackRange:
+			PlayerSeen = true
+			if !isAttacking:
+				MoveTowardsPlayer()
+		if MyPlayerTracker.PlayerRelativeDistance <= SightRange && MyPlayerTracker.PlayerRelativeDistance <= AttackRange:
+			if !isAttacking:
+				TriggerAttackAnimation()
+		if MyPlayerTracker.PlayerRelativeDistance >= SightRange && MyPlayerTracker.PlayerRelativeDistance >= AttackRange:
+			if PlayerSeen:
+				MoveTowardsPlayer()
+
+func MoveTowardsPlayer() -> void:
+	# Get the Player's direction and handle the movement/deceleration.
+	var direction := Vector3( PlayerNode.position.x - position.x, 0, PlayerNode.position.z - position.z)
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+
+
+	
+
+func TriggerAttackAnimation() -> void:
+	isAttacking = true
+	await get_tree().create_timer(AttackDelay, false, false, true).timeout
+	if MyPlayerTracker.PlayerRelativeDistance <= AttackRange:
+		DealDamageToPlayer()
+	if MyPlayerTracker.PlayerRelativeDistance > AttackRange:
+		print("I Missed!")
+	isAttacking = false
+
+func DealDamageToPlayer() -> void:
+	print("I Hit!")
+	pass

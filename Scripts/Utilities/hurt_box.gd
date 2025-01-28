487f96815888cdce51af9f470909b6e4c9679abb
Nfavia <Nfavia1297@gmail.com>
2025-01-27 19:31:42 -0600

    Weapon work

--- Scripts/Utilities/hurt_box.gd
+++ Scripts/Utilities/hurt_box.gd
@@ -2,6 +2,8 @@ class_name HurtBox extends Area3D
 
 @export var damage : int = 0 ## Should be changed based on weapon or enemy
 
+signal weapon_hit ## Signal emitted when player hit's an enemy with weapon
+
 func _ready() -> void:
 	area_entered.connect(area_hit)
 
@@ -9,3 +11,5 @@ func _ready() -> void:
 func area_hit( area : Area3D ) -> void:
 	if area is HitBox:
 		area.take_damage( damage )
+	if area.get_parent().is_in_group("Enemy"):
+		weapon_hit.emit()
